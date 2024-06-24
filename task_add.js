9fc86e66ce9651ff733e01e1bb0b87a687285220
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-06-24 16:34:49 +0900

    tasklistの要素の取得タイミング変更
    変更前：追加ボタンクリック時
    変更後：javascript読み込み時

--- task_add.js
+++ task_add.js
@@ -3,11 +3,11 @@ let taskValue = document.querySelector('.task_value');//タスクの名前を取
 let taskDate = document.querySelector('.task_date');//タスクの期日を取得
 let taskImportance = document.querySelector('.task_importance');//タスクの優先度を取得
 let taskSubmit = document.querySelector('.task_submit');//追加ボタンのクリックを確認
+let taskList = document.querySelector('.task_list');
 
 //追加ボタンをクリックした時の動作(タスク一覧にタスクを追加)
 taskSubmit.addEventListener('click',() => {
-    //タスクのフォーム作成
-    let taskList = document.querySelector('.task_list');
+    //タスク作成
     let newTask = document.createElement('div');
     newTask.classList.add('newtask');//クラス名を追加
     taskList.appendChild(newTask);
