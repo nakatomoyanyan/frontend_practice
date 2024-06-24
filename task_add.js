bd1c50744664a826c537a9475036198779f4f15f
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-06-24 16:15:44 +0900

    タスクの中身(タスク名、期日、優先度)をまとめて追加
    inner.HTMLのメソッドを用いてタスクの中身をまとめて追加するように書き換えました。

--- task_add.js
+++ task_add.js
@@ -11,18 +11,12 @@ taskSubmit.addEventListener('click',() => {
     let newTask = document.createElement('div');
     newTask.classList.add('newtask');//クラス名を追加
     taskList.appendChild(newTask);
-    //タスク名を追加
-    let newTaskValue = document.createElement('p');
-    newTaskValue.innerText = taskValue.value;
-    newTask.appendChild(newTaskValue);
-    //タスクの期日を追加
-    let newTaskDate = document.createElement('p');
-    newTaskDate.innerText = '期限日:' + taskDate.value;
-    newTask.appendChild(newTaskDate);
-    //タスクの優先度を追加
-    let newTaskImportance = document.createElement('p');
-    newTaskImportance.innerText = '優先度:' + taskImportance.value;
-    newTask.appendChild(newTaskImportance);
+    //タスクの中身を追加
+    newTask.innerHTML = `
+        <p>${taskValue.value}</p>
+        <p>期限日:${taskDate.value}</p>
+        <p>優先度:${taskImportance.value}</p>
+    `
     //タスク入力フォームを初期化
     taskValue.value = '';
     taskDate.value = '';
